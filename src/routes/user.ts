ee5c0eafd39928ddb7dcd54c2e0d04fa73637c06
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-11-05 19:08:21 +0100

    wip

---
+++ src/routes/user.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const route = express.Router()
+
+route.post("/login", () => {
+    // TODO - get login from page
+})
+
+route.get("/:user_id", () => {
+    // TODO - make use of the user_id
+})
+
