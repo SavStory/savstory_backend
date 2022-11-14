c79f20dd39d82106c7b4e606f88702b9b4da0901
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-11-14 10:16:47 +0100

    feat: eslint/prettier plugin

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,7 +1,6 @@
-import {model} from "mongoose"
-import {salvationSttorySchema} from "./salvation_story"
-import {userSchema} from "./users"
+import { model } from 'mongoose'
+import { salvationSttorySchema } from './salvation_story'
+import { userSchema } from './users'
 
-export const SalvationStory = model("salvation_story", salvationSttorySchema)
-export const User = model("user", userSchema)
-
+export const SalvationStory = model('salvation_story', salvationSttorySchema)
+export const User = model('user', userSchema)
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,44 +1,42 @@
-import express from "express"
-import {User} from "../model/index"
-import {expressjwt} from "express-jwt"
-const route = express.Router()
+import express from "express";
+import { User } from "../model/index";
+import { expressjwt } from "express-jwt";
+const route = express.Router();
 
 // login user
 route.post("/login", async (req, res) => {
-    const {username, password} = req.body
+  const { username, password } = req.body;
 
-    if (!username || !password) {
-        res.status(400).json({
-            error: "username or password should not be empty"
-        })
-    }
+  if (!username || !password) {
+    res.status(400).json({
+      error: "username or password should not be empty",
+    });
+  }
 
-    console.log(req.body)
-    res.json({
-        message: "user successfully logged"
-    })
-})
+  console.log(req.body);
+  res.json({
+    message: "user successfully logged",
+  });
+});
 
 // register user
 route.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({
+      error: "username or password should not be empty",
+    });
+  }
 
-    const {username, password} = req.body
-    if (!username || !password) {
-        res.status(400).json({
-            error: "username or password should not be empty"
-        })
-    }
+  const existingUser = await User.findOne({ username: req.body.username });
 
-    const existingUser = await User.findOne({username: req.body.username})
+  if (existingUser) {
+    res.status(200).json({
+      message: "",
+    });
+  }
+});
 
-    if (existingUser) {
-        res.status(200).json({
-            message: ""
-        })
-    }
-})
+route.get("/:user_id", () => {});
 
-route.get("/:user_id", () => {
-})
-
-export default route
+export default route;
