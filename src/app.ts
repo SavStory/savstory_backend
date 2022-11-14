33a6371ff4ede342e4efccae72d58269a7d94b20
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-11-14 10:18:14 +0100

    format files with prettier

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express from "express"
-import http from "http"
-import chalk from "chalk"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import logger from "morgan"
-import cors, {CorsOptions} from "cors"
-import users from "./routes/user"
-import bodyParser from "body-parser"
+import express from 'express'
+import http from 'http'
+import chalk from 'chalk'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import logger from 'morgan'
+import cors, { CorsOptions } from 'cors'
+import users from './routes/user'
+import bodyParser from 'body-parser'
 
 dotenv.config()
 const app = express()
@@ -15,34 +15,33 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer(app)
 
 const corOptions: CorsOptions = {
-    origin: "*"
+  origin: '*'
 }
 
 // middlewares
-app.use(logger("dev"))
+app.use(logger('dev'))
 app.use(cors(corOptions))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // routes
-app.use("/api", users)
+app.use('/api', users)
 
 async function main() {
-    if (process.env.CONNECTION_STRING) {
-       await mongoose.connect(process.env.CONNECTION_STRING)
-       console.log(chalk.greenBright("-- database connected"))
-    }
+  if (process.env.CONNECTION_STRING) {
+    await mongoose.connect(process.env.CONNECTION_STRING)
+    console.log(chalk.greenBright('-- database connected'))
+  }
 
-    server.listen(PORT,() => {
-        console.log(chalk.greenBright(`-- server running on port: ${PORT}`))
-    })
-    
-    server.on("error", error => {
-        console.log(chalk.redBright("-- error starting server: ", error.message))
-    })
+  server.listen(PORT, () => {
+    console.log(chalk.greenBright(`-- server running on port: ${PORT}`))
+  })
+
+  server.on('error', (error) => {
+    console.log(chalk.redBright('-- error starting server: ', error.message))
+  })
 }
 
-
-main().catch(err => {
-    console.log(chalk.redBright(err.message))
-}) 
+main().catch((err) => {
+  console.log(chalk.redBright(err.message))
+})
--- src/model/salvation_story.ts
+++ src/model/salvation_story.ts
@@ -1,21 +1,23 @@
-import { Schema } from "mongoose";
+import { Schema } from 'mongoose'
 
-export const salvationSttorySchema = new Schema({
+export const salvationSttorySchema = new Schema(
+  {
     author: String, // string to the user id
     body: String,
     likes: Number,
     date_of_salvation: Date,
     venue: String,
-    comments: [{body: String, date: Date}]
-}, {
+    comments: [{ body: String, date: Date }]
+  },
+  {
     statics: {
-        getSalvationStory(userID) {
-            // TODO - using the user ID, get the salvation story of the user
-        }, 
+      getSalvationStory(userID) {
+        // TODO - using the user ID, get the salvation story of the user
+      },
 
-        getSalvationStoriesField () {
-            // returns an array of the salvation story
-        }
+      getSalvationStoriesField() {
+        // returns an array of the salvation story
+      }
     }
-})
-
+  }
+)
